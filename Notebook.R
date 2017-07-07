7f86174260628649b6cab964ec4c2f4c8a350a59
olha-melnyk <olhamelnyk@gmail.com>
2017-07-07 16:41:56 +0300

    Added find the percentage of NAs in a data.frame

--- Notebook.R
+++ Notebook.R
@@ -1,6 +1,14 @@
 library(gdata)
 data_PPD = read.xls (xls = "Data.xlsx", sheet = 2, blank.lines.skip = TRUE, check.names = FALSE)
-data_PPD_1995_2015 = data_PPD[,37:57]
+data_PPD_1995_2015 <- data_PPD[,-1]
+rownames(data_PPD_1995_2015) <- data_PPD[,1]
+data_PPD_1995_2015 <- data_PPD_1995_2015[,36:56]
 data_PPD_1995_2015
 colMeans(is.na(data_PPD_1995_2015)) #percentage of NAs in a data.frame
 colMeans(!is.na(data_PPD_1995_2015))
+data_row_PPD = dcast(melt(data_PPD_1995_2015), variable ~ rownames(data_PPD_1995_2015))
+ibrary(plyr)
+data_row_PPD <- rename(data_row_PPD, c("variable"="Year"))
+data_row_PPD
+colMeans(is.na(data_row_PPD))
+colMeans(!is.na(data_row_PPD))
