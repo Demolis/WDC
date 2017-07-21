6e951b5dee4220c3169b002a980d80cd62e2d3b9
olha-melnyk <olhamelnyk@gmail.com>
2017-07-21 11:46:54 +0300

    Added explanation to variables

--- Notebook.R
+++ Notebook.R
@@ -1,28 +1,22 @@
 library(gdata)
-data_PPD = read.xls (xls = "Data.xlsx", sheet = 2, blank.lines.skip = TRUE, check.names = FALSE)
-data_PPD_1995_2015 <- data_PPD[,-1]
-rownames(data_PPD_1995_2015) <- data_PPD[,1]
-data_PPD_1995_2015 <- tail(data_PPD_1995_2015, 36)
-data_PPD_1995_2015
-colMeans(is.na(data_PPD_1995_2015)) #percentage of NAs in a data.frame
-colMeans(!is.na(data_PPD_1995_2015))
 library(reshape2)
-data_row_PPD = dcast(melt(data_PPD_1995_2015), variable ~ rownames(data_PPD_1995_2015))
 library(plyr)
-data_row_PPD <- rename(data_row_PPD, c("variable"="Year"))
-data_row_PPD
-colMeans(is.na(data_row_PPD))
-colMeans(!is.na(data_row_PPD))
-del_data_PPD_col <- data_row_PPD[, which(colMeans(!is.na(data_row_PPD)) >= 0.75)]
 
-#function
-data_set<- function(n, p){
-     data_ind_code = read.xls (xls = "Data.xlsx", sheet = n, blank.lines.skip = TRUE, check.names = FALSE)
-     data_ind_code_1995_2015 <- data_PPD[,-1]
-     rownames(data_ind_code_1995_2015) <- data_ind_code[,1]
-     data_ind_code_1995_2015 <- data_ind_code_1995_2015[,36:53]
-     data_row_ind_code = dcast(melt(data_ind_code_1995_2015), variable ~ rownames(data_ind_code_1995_2015))
-     data_row_ind_code <- rename(data_row_ind_code, c("variable"="Year"))
-     del_data_na <- data_row_ind_code[, which(colMeans(!is.na(data_row_ind_code)) >= p)]
-     print(del_data_na)
-  }
+#n - номер листа в таблице .xlsx
+#p - это процент данных, который должен быть по одному показателю.
+#xls - название файла, в котором хранятся данные
+
+data_set<- function(n, p, xls){
+       data_ind_code = read.xls (xls, sheet = n, blank.lines.skip = TRUE, check.names = FALSE)
+       data_ind_code_years <- data_ind_code[,-1]
+       #data for function from 1995 till last year
+       rownames(data_ind_code_years) <- data_ind_code[,1]
+       data_ind_code_years <- data_ind_code_years[,36:53]
+       data_row_ind_code = dcast(melt(data_ind_code_years), variable ~ rownames(data_ind_code_years))
+       data_row_ind_code <- rename(data_row_ind_code, c("variable"="Year"))
+       del_data_na <- data_row_ind_code[, which(colMeans(!is.na(data_row_ind_code)) >= p)]
+       #del_data_na - результат (набор данных)
+       print(del_data_na)
+}
+
+data_set(2, 0.5, "Data.xlsx")
